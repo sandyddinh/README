87678ee8acd14ebf7f9b7af496b18c3edc31b03a
Sandy <sandyddinh@gmail.com>
2020-12-06 16:36:32 -0500

    html structure, team object, card set, button events

--- js/app.js
+++ js/app.js
@@ -1,1 +1,69 @@
-console.log($)
+class Team {
+    constructor(teamName, player1, player2, player3){
+        this.teamName = teamName;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player4 = player3;
+        this.points = 0;
+        this.strikes = 0;
+    }
+}
+
+const teamUno = new Team('Team Uno', 'sandy', 'tina', 'judy');
+console.log(teamUno);
+
+const questionCards = [
+    {
+        question: "What is the most popular tv show of 2020?",
+        answer: [
+            ["Queens Gambit", 45],
+            ["The Undoing", 30],
+            ["The Last Dance", 25],
+            ["The Bachelor", 23],
+            ["The Crown", 17],
+            ["This is Us", 15],
+            ["Emily in Paris", 10],
+            ["Grey's Anatomy", 8]
+        ]
+    }
+]
+
+
+$(() => {
+    const $startRound = $('#start-round');
+    const $question = $('.question');
+    const $form = $('form');
+    const $input = $('#answer-box');
+    const $points = $('.points');
+    const $score1 = $('.score1');
+    const $score2 = $('score2');
+    const $strike1 = $('strike1');
+    const $strike2 = $('strike2');
+    const $answer1 = $('answer1');
+    const $answer2 = $('answer2');
+    const $answer3 = $('answer3');
+    const $answer4 = $('answer4');
+    const $answer5 = $('answer5');
+    const $answer6 = $('answer6');
+    const $answer7 = $('answer7');
+    const $answer8 = $('answer8');
+    let totalPoints = 0;
+    let cardSet = 0;
+
+    $startRound.on('click', (event) => {
+        event.preventDefault();
+        console.log('started the round');
+        
+        $question.html(`${questionCards[cardSet].question}`);
+
+    })
+
+    $form.on('submit', (event) => {
+        event.preventDefault();
+        console.log('submitted answer');
+        if($input.val() === questionCards[cardSet].answer[0][0]){
+            totalPoints += questionCards[cardSet].answer[0][1];
+            $points.html(`${totalPoints}`);
+        }
+    })
+})
